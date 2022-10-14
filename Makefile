e83be0abafbdfc1f3690c39d92092dc4f351f590
Gabe Susman <36492747+Max-Gabriel-Susman@users.noreply.github.com>
2022-10-13 22:40:13 -0600

    fixed build target, reverted back to old handler pattern, got init

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 build:
 	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o main main.go
-	zip main.zip main.go
+	zip main.zip main
+
+# curl --location --request POST '$YOUR_API_ENDPOINT' 
+post:
+	curl --location --request POST '' \
+	--header 'Content-Type: application/json' \
+	--data-raw '{"a":"b"}'
--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
+	run()
+}
+
+func run() {
 	lambda.Start(handler)
 }
 
 func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	log.Println("Goliath Online")
-
-	response := events.APIGatewayProxyResponse{
-		StatusCode: 209,
-	}
-
-	return &response, nil
+	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
+	resp.StatusCode = 200
+	resp.Body = fmt.Sprintf(`{"request_payload":%s}`, req.Body)
+	return &resp, nil
 }
